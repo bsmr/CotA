2c47516a50dff5f79514ae959b8345e6758853e2
Ivan Trombley <itrombley@dot-borg.org>
2018-01-29 10:00:43 -0800

    Changed dateSortable to return an ulong.

--- source/util.d
+++ source/util.d
@@ -43,17 +43,17 @@ string htmlColor(RGBA color, double opacity = 1.0)
 /**
  * Convert a SotA logfile date string into a sortable format.
  */
-string dateSortable(string date)
+ulong dateSortable(string date)
 {
   // Split the string into the date, time and am/pm components.
   immutable auto components = date.split();
   if (components.length != 3)
-    return [];
+    return 0;
 
   // Split the date component into the year, month and day parts.
   immutable auto dateParts = components[0].split("/");
   if (dateParts.length != 3)
-    return [];
+    return 0;
 
   // Convert the year, month and day to integers.
   immutable auto month = to!(int)(dateParts[0]);
@@ -63,7 +63,7 @@ string dateSortable(string date)
   // Split the time component into the hour, minute and second parts.
   immutable auto timeParts = components[1].split(":");
   if (timeParts.length != 3)
-    return [];
+    return 0;
 
   // Convert the hour, minute and second to integers and convert to 24 hour
   // format.
@@ -71,8 +71,9 @@ string dateSortable(string date)
   immutable auto minute = to!(int)(timeParts[1]);
   immutable auto second = to!(int)(timeParts[2]);
 
-  // Recombine the elements into a sortable string.
-  return format!("%04d%02d%02d%02d%02d%02d")(year, month, day, hour, minute, second);
+  // Convert the values into one big sortable ulong; 
+  return year * (10UL ^^ 10) + month * (10UL ^^ 8) + day * (10UL ^^ 6) + hour * (
+      10UL ^^ 4) + minute * (10UL ^^ 2) + second;
 }
 
 /**
